f8a12dd1fd8dd6ea470646aba7cde2814de11aba
ethankook <93116378+ethankook@users.noreply.github.com>
2025-05-05 20:02:12 -0700

    minor bug

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,31 +6,34 @@ function init() {
   // TODO
   const hornSelect = document.getElementById("horn-select");
 
-  const audioSelect = document.getElementsById("audio.hidden");
+  const audioElement = document.getElementsByClassName("hidden");
   const volumeSelect = document.getElementById("volume");
   const volumeImg = document.querySelector('img[alt="Volume level 2"]');
-  let volumeValue;
+
+
+  const jsConfetti = new JSConfetti();
+
 
   hornSelect.addEventListener("change", (event) => {
     const img = document.querySelector('img[alt="No image selected"]');
     
     if(event.target.value == 'air-horn') {
       img.src = "assets/images/air-horn.svg";
-      audioSelect.src = "assets/audio/air-horn.mp3";
+      audioElement.src = "assets/audio/air-horn.mp3";
     }
     else if(event.target.value == 'car-horn') {
       img.src = "assets/images/car-horn.svg";
-      audioSelect.src = "assets/audio/car-horn.mp3";
+      audioElement.src = "assets/audio/car-horn.mp3";
     }
     else if(event.target.value == 'party-horn') {
       img.src = "assets/images/party-horn.svg"
-      audioSelect.src = "assets/audio/party-horn.mp3";
+      audioElement.src = "assets/audio/party-horn.mp3";
     }
 
   });
 
   volumeSelect.addEventListener("change", (event) => {
-    volumeValue = event.target.value;
+    const volumeValue = event.target.value;
     if (event.target.value == 0) {
       volumeImg.src = "assets/icons/volume-level-0.svg";
     }
@@ -47,13 +50,11 @@ function init() {
   });
 
   document.querySelector('button').addEventListener("click", (event) => {
-    if(event.target.value == 'party-horn') {
-      const jsConfetti = new JSConfetti();
+    if(hornSelect.value == 'party-horn') {
       jsConfetti.addConfetti();
     }
-    audioSelect.volume = volumeValue / 100;
-    audioSelect.play();
-    console.log(audioSelect.src);
+    audioElement.volume = volumeValue / 100;
+    audioElement.play();
 
   });
 
